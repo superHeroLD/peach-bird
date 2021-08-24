7e51a1e8f7978ba973fd66ac3870edbb881389e2
superHeroLD <superhero_L@163.com>
2021-08-24 20:33:00 +0800

    1.[M] Modify gitignore 2.[M] Add two Class

--- src/main/java/com/ld/peach/bird/Train.java
+++ src/main/java/com/ld/peach/bird/Train.java
@@ -1,11 +1,50 @@
 package com.ld.peach.bird;
 
+
+import ai.djl.Model;
+import ai.djl.modality.rl.env.RlEnv;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Train Bird
  *
  * @author ld
  */
 public class Train {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Train.class);
+
+    /**
+     * gameSteps to observe before training
+     */
+    public static final int OBSERVE = 1000;
+    /**
+     * frames over which to anneal epsilon
+     */
+    public static final int EXPLORE = 3000000;
+    /**
+     * save model every 100,000 step
+     */
+    public static final int SAVE_EVERY_STEPS = 100000;
+    /**
+     * number of previous transitions to remember
+     */
+    public static final int REPLAY_BUFFER_SIZE = 50000;
+    /**
+     * decay rate of past observations
+     */
+    public static final float REWARD_DISCOUNT = 0.9f;
+    public static final float INITIAL_EPSILON = 0.01f;
+    public static final float FINAL_EPSILON = 0.0001f;
+    public static final String PARAMS_PREFIX = "dqn-trained";
+
+    /**
+     * An environment to use for reinforcement learning.
+     */
+    static RlEnv.Step[] batchSteps;
+
+    private Train() {}
 
     public static void main(String[] args) {
 
---
+++ src/main/java/com/ld/peach/bird/utils/Arguments.java
@@ -0,0 +1,81 @@
+package com.ld.peach.bird.utils;
+
+import lombok.Getter;
+import org.apache.commons.cli.*;
+
+import static com.ld.peach.bird.utils.Constant.*;
+
+/**
+ * 这个类负责对从命令行中获取的参数进行解析
+ * 参数有如下四种 使用的时候使用简写就可以了
+ * <p>
+ * -p
+ * -g 是否展示图像
+ * -t
+ * -b
+ *
+ * @author ld
+ */
+public class Arguments {
+
+    @Getter
+    private final int batchSize;
+    @Getter
+    private final boolean graphics;
+    @Getter
+    private final boolean preTrained;
+    @Getter
+    private final boolean testing;
+
+
+    public Arguments(CommandLine cmd) {
+        graphics = cmd.hasOption(GRAPHICS_PARAM);
+
+        if (cmd.hasOption(BATCH_SIZE_PARAM)) {
+            batchSize = Integer.parseInt(cmd.getOptionValue(BATCH_SIZE_PARAM));
+        } else {
+            batchSize = 32;
+        }
+
+        preTrained = cmd.hasOption(PRE_TRAINED_PARAM);
+
+        testing = cmd.hasOption(TESTING_PARAM);
+    }
+
+    public static Arguments parseArgs(String[] args) throws ParseException {
+        Options options = Arguments.getOptions();
+        DefaultParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args, null, false);
+        return new Arguments(cmd);
+    }
+
+    public static Options getOptions() {
+        Options options = new Options();
+        options.addOption(
+                Option.builder("g")
+                        .longOpt(GRAPHICS_PARAM)
+                        .argName("GRAPHICS")
+                        .desc("Training with graphics")
+                        .build());
+        options.addOption(
+                Option.builder("b")
+                        .longOpt(BATCH_SIZE_PARAM)
+                        .hasArg()
+                        .argName("BATCH-SIZE")
+                        .desc("The batch size of the training data.")
+                        .build());
+        options.addOption(
+                Option.builder("p")
+                        .longOpt(PRE_TRAINED_PARAM)
+                        .argName("PRE-TRAINED")
+                        .desc("Use pre-trained weights")
+                        .build());
+        options.addOption(
+                Option.builder("t")
+                        .longOpt(TESTING_PARAM)
+                        .argName("TESTING")
+                        .desc("test the trained model")
+                        .build());
+        return options;
+    }
+}
